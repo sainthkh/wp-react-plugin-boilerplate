ed08fb679430ee434ee1447bdc0875f5df16b0b1
KHeo <sainthkh@naver.com>
2021-02-05 12:47:53 +0900

    feat: Add runSQL e2e util.

--- test/e2e/api/db.php
+++ test/e2e/api/db.php
@@ -15,6 +15,16 @@ function wp_react_db_api() {
 			'permission_callback' => '__return_true',
 		)
 	);
+
+	register_rest_route(
+		'wp-react/test/v1',
+		'/run-sql',
+		array(
+			'methods'  => 'POST',
+			'callback' => 'wp_react_plugin_run_sql',
+			'permission_callback' => '__return_true',
+		)
+	);
 }
 
 add_action( 'rest_api_init', 'wp_react_db_api' );
@@ -30,4 +40,20 @@ function wp_react_plugin_reset_table(WP_REST_Request $request) {
 	}
 
 	return "Reset succeeded";
+}
+
+define( 'SQL_FILE_ROOT', dirname( __DIR__ ) . '/sql/' ); /* => test/e2e/sql */
+
+function wp_react_plugin_run_sql(WP_REST_Request $request) {
+	global $wpdb;
+
+	$files = json_decode($request->get_body(), true);
+
+	foreach($files as $file) {
+		$raw_sql = file_get_contents( SQL_FILE_ROOT . $file );
+		$sql = $wpdb->prepare($raw_sql, array());
+		$wpdb->query($sql);
+	}
+
+	return "SQL run succeeded";
 }
---
+++ test/e2e/sql/react.sql
@@ -0,0 +1,2 @@
+insert into wp_posts (post_author, post_content, post_title, post_status, post_name)
+values (1, '[React]', 'Hello world', 'published', 'i');
