78e44dc5466a7bbf8bc2946f2748e122baab8a25
KHeo <sainthkh@naver.com>
2021-02-17 12:08:52 +0900

    test: deps are correctly added.

---
+++ packages/e2e-tests/specs/deps.spec.ts
@@ -0,0 +1,38 @@
+import {
+	visitAdminPage,
+	// No definition.
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+} from '@wordpress/e2e-test-utils';
+
+describe( 'WP Dependencies', () => {
+	describe( 'Plugin', () => {
+		it( 'From official WP Plugin repo', async () => {
+			visitAdminPage( 'plugins.php' );
+
+			// Wait for load
+			await page.waitForSelector( '.wp-heading-inline' );
+
+			// Check if the plugin is installed correctly.
+			const [ name ] = await page.$x(
+				"//strong[contains(., 'Hello Samuel L Jackson')]"
+			);
+
+			expect( name ).not.toBeUndefined();
+		} );
+	} );
+
+	describe( 'Theme', () => {
+		it( 'From official WP Themes repo', async () => {
+			visitAdminPage( 'themes.php' );
+
+			// Wait for load
+			await page.waitForSelector( '.wp-heading-inline' );
+
+			// Check if the plugin is installed correctly.
+			const name = await page.$( '#twentyfourteen-name' );
+
+			expect( name ).not.toBeNull();
+		} );
+	} );
+} );
