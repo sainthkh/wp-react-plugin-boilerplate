78173f03cadcb37bdd18a4aa982c14b82ad19dab
KHeo <sainthkh@naver.com>
2021-03-03 10:12:39 +0900

    detect production option.

--- bin/install-deps/exe.sh
+++ bin/install-deps/exe.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo $1
 export WP_REACT_GITHUB_SECRET=$1
+
+for var in "$@"
+do
+    if [ "$var" = "--production" ]
+	then
+		export WP_REACT_PRODUCTION=$var
+	fi
+done
+
 php wp-content/plugins/wp-react-plugin-boilerplate/bin/install-deps/main.php
